0dc810ecf039992373fe31c8a8acfb03ece7bcaf
SOOOOOui <soo.zhan.thong@student.mmu.edu.my>
2024-06-26 15:46:42 +0800

    aa

---
+++ tryingjs.js
@@ -0,0 +1,10 @@
+function gogo(){
+    document.getElementById('working').innerHTML = "THIS IS WAY TOO IMPORTANT";
+}
+
+function multiply(){
+    document.getElementById('multi').innerHTML = 10*10;
+}
+
+var ming = 10;
+let gogog = "This is great!";
